c3e01bb42ab81b7fb916a9b34670951f886c073e
carlodu00 <carlosdu.carloseduardo@gmail.com>
2024-12-05 13:59:57 -0300

    .env corrgido

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -2,21 +2,33 @@
 
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/database';
+import {
+  EXPO_FIREBASE_API_KEY,
+  EXPO_FIREBASE_AUTH_DOMAIN,
+  EXPO_FIREBASE_DATABASE_URL,
+  EXPO_FIREBASE_PROJECT_ID,
+  EXPO_FIREBASE_STORAGE_BUCKET,
+  EXPO_FIREBASE_MESSAGING_SENDER_ID,
+  EXPO_FIREBASE_APP_ID
+} from '@env';
 
 // Substitua pelos valores do seu objeto firebaseConfig
 const firebaseConfig = {
-  apiKey: "AIzaSyBpYdH3aL353T4zutMaSA6wUMTniTwbii4",
-  authDomain: "empacotador-inteligente.firebaseapp.com",
-  databaseURL: "https://empacotador-inteligente-default-rtdb.firebaseio.com",
-  projectId: "empacotador-inteligente",
-  storageBucket: "empacotador-inteligente.firebasestorage.app",
-  messagingSenderId: "1094951758398",
-  appId: "1:1094951758398:web:d755ef4477415c07697f6f"
+  apiKey: EXPO_FIREBASE_API_KEY,
+  authDomain: EXPO_FIREBASE_AUTH_DOMAIN,
+  databaseURL: EXPO_FIREBASE_DATABASE_URL,
+  projectId: EXPO_FIREBASE_PROJECT_ID,
+  storageBucket: EXPO_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: EXPO_FIREBASE_MESSAGING_SENDER_ID,
+  appId: EXPO_FIREBASE_APP_ID,
 };
 
 // Inicializa o Firebase apenas se ainda não houver uma instância inicializada
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
+
+ console.log("o aoutput é")
+  console.log(firebaseConfig.databaseURL)
 
 export default firebase;
